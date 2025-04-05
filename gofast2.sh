3e070988ef4694ce3d7127d733ca4b4cdbf7d5dc
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-04-04 20:34:17 -0500

    more flags no isystem nostdinc++

--- gofast2.sh
+++ gofast2.sh
@@ -55,7 +55,7 @@ cmake -G Ninja "${SRC_DIR}/llvm-project/llvm" \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_C_VISIBILITY_PRESET=hidden \
     -DCMAKE_C_FLAGS="-static -march=native -Os -g0 " \
-    -DCMAKE_CXX_FLAGS="-static -march=native -Os -g0 -nostdinc++ -isystem /usr/include/c++/v1 " \
+    -DCMAKE_CXX_FLAGS="-static -march=native -Os -g0 " \
     -DCMAKE_EXE_LINKER_FLAGS="-static" \
     -DCMAKE_HOST_TRIPLE="x86_64-linux-llvm" \
     -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF \
